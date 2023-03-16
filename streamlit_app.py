be86c5c36458a2dbc594f0327fed6322cb677da7
Daniele <d_abbatelli@hotmail.com>
2023-03-16 15:17:29 +0100

    Added save to Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector
 
+st.set_page_config(layout="wide")
+
 st.title('Editable dataframe test app')
 st.text('This app just allows you to edit a large dataset 📝 (60 cols x 200k rows) hosted on Snowflake ❄️')
 
@@ -16,13 +18,19 @@ def get_from_snowflake():
         con.close()
     return df
 
-def save_to_snowflake():
-    pass
+def save_to_snowflake(df):
+    con = snowflake.connector.connect(**st.secrets["snowflake"]
+    try:
+        cur.execute('TRUNCATE TABLE SYNTETIC_DATASET_T')
+        success, nchunks, nrows, output = write_pandas(con, df, 'SYNTETIC_DATASET_T')
+    finally:
+        con.close()
+    return success
 
 df = get_from_snowflake()
 
 edited_df = st.experimental_data_editor(df, key='data_editor')
 st.write(st.session_state["data_editor"])
 
-# if streamlit.button('Save to Snowflake!'):
-#     save_to_snowflake()
+if streamlit.button('Save to Snowflake!'):
+    save_to_snowflake()
