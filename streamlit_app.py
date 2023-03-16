67cccbc3f8555d8c81cfb8a3c2c19b2e5fdb4105
Daniele <d_abbatelli@hotmail.com>
2023-03-16 15:22:26 +0100

    Bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,13 @@ def get_from_snowflake():
 def save_to_snowflake(df):
     con = snowflake.connector.connect(**st.secrets["snowflake"]
     try:
+        cur = con.cursor()
         cur.execute('TRUNCATE TABLE SYNTETIC_DATASET_T')
         success, nchunks, nrows, output = write_pandas(con, df, 'SYNTETIC_DATASET_T')
 
         if success:
             st.session_state['data_editor'] = {}
-            
+
     finally:
         con.close()
     return success
@@ -38,4 +39,4 @@ edited_df = st.experimental_data_editor(df, key='data_editor')
 st.write(st.session_state["data_editor"])
 
 if streamlit.button('Save to Snowflake!'):
-    save_to_snowflake()
+    save_to_snowflake(edited_df)
